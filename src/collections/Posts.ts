b04aa5eefdbb631c233b6138983fc3d7c4f54c6b
Abdul Fattah Ikhsan <skull.saders18@gmail.com>
2023-03-13 08:41:55 +0700

    feat: #1 finish publishing the schedule logic

--- src/collections/Posts.ts
+++ src/collections/Posts.ts
@@ -1,4 +1,52 @@
-import { CollectionConfig } from 'payload/types';
+import type Agenda from 'agenda';
+import dayjs from 'dayjs';
+import { Post } from 'payload/generated-types';
+import {
+  CollectionAfterChangeHook,
+  CollectionBeforeChangeHook,
+  CollectionConfig,
+} from 'payload/types';
+
+const afterChangeHook: CollectionAfterChangeHook<Post> = async ({req, operation, doc, previousDoc}) => {
+  switch (operation) {
+    case 'create':
+    if (doc.status === 'draft' && doc.publishedSchedule && dayjs(doc.publishedSchedule).format() > dayjs().format()) {
+        const agenda = req.app.get('agenda') as Agenda;
+        await agenda.schedule(dayjs(doc.publishedSchedule).format(), 'publish-post', {
+          id: doc.id,
+          title: doc.title
+        })
+      }
+      break;
+      case 'update':
+      if (doc.status === 'draft' && doc.publishedSchedule && dayjs(doc.publishedSchedule).format() > dayjs().format()) {
+        const agenda = req.app.get('agenda') as Agenda;
+        let agendaJobs = await agenda.jobs({
+          name: 'publish-post',
+          'data.id': doc.id,
+        }, {_id: 1}, 1);
+        if (agendaJobs.length === 1) {
+          await agendaJobs[0].remove();
+        }
+        await agenda.schedule(dayjs(doc.publishedSchedule).format(), 'publish-post', {
+          id: doc.id,
+          title: doc.title
+        })
+      }
+      break;
+  
+    default:
+      break;
+  }
+  return doc;
+}
+
+const beforeChangeHook: CollectionBeforeChangeHook<Post> = async ({data, req, operation, originalDoc}) => {
+  if (data.status === 'published') {
+    data.publishedDate = dayjs().format();
+  }
+  return data;
+}
 
 const Posts: CollectionConfig = {
   slug: 'posts',
@@ -8,6 +56,10 @@ const Posts: CollectionConfig = {
   },
   access: {
     read: () => true,
+  },
+  hooks: {
+    beforeChange: [beforeChangeHook],
+    afterChange: [afterChangeHook]
   },
   fields: [
     {
@@ -22,6 +74,21 @@ const Posts: CollectionConfig = {
     {
       name: 'publishedDate',
       type: 'date',
+      admin: {
+        hidden: true,
+      }
+    },
+    {
+      name: 'publishedSchedule',
+      type: 'date',
+      required: false,
+      admin: {
+        position: 'sidebar',
+        date: {
+          pickerAppearance: "dayAndTime",
+          timeIntervals: 5,
+        }
+      }
     },
     {
       name: 'category',
--- src/jobs/definitions.ts
+++ src/jobs/definitions.ts
@@ -4,7 +4,12 @@ import { type Payload } from 'payload';
 const publishPostDefinition = (agenda: Agenda, payload: Payload) => {
     agenda.define('publish-post', {shouldSaveResult: true}, async (job) => {
         payload.logger.info(job.attrs, 'agenda.define publish-post job');
-        // TODO: extract data & update post status from `draft` to `published`
+        const { attrs: { data: { id } } } = job;
+        let post = await payload.findByID({collection: 'posts', id, depth: 2});
+        payload.logger.info({post}, 'agenda.define publish-post Post');
+        if (post?.status === 'draft') {
+            await payload.update({collection: 'posts', id, data: {status: 'published'}, depth: 2});
+        }
     })
 }
 
